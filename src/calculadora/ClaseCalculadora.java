6d075f9e05c479836a645832944eb63aee95701b
jorge6217 <jorge6217@gmail.com>
2023-02-22 20:10:43 -0600

    class IStack and IList

--- src/calculadora/ClaseCalculadora.java
+++ src/calculadora/ClaseCalculadora.java
@@ -1,5 +1,51 @@
 package calculadora;
 
-public class ClaseCalculadora {
+import java.util.ArrayList;
+import java.util.EmptyStackException;
 
+public class ClaseCalculadora implements ICalculator {
+	clasestack stack = null;
+    public Clasecalculadora(){
+        stack = new clasestack();
+    }
+    public double parsear(String expression){
+        return Double.parseDouble(expression);
+    }
+    public String remover(String s){
+        return s.replace(" ", "");
+    }
+    public double evaluate(String ope) {
+        String expression = ope;
+        int len = expression.length();
+        double res = 0.0;
+        for (int i = 0; i < len; i++){
+            char c = expression.charAt(i);
+            String co = String.valueOf(c);
+            if (c != '\n'){
+                if(Character.isDigit(c)){
+                    stack.push(parsear(co));
+                }
+                else{
+                    double a = stack.pop(), b = stack.pop();
+                    if (c == '+'){
+                        res = a + b;
+                    }
+                    if (c == '-'){
+                        res = a - b;
+                    }   
+                    if (c == '/'){
+                        res = a / b;
+                    }
+                    if (c == '*'){
+                        res = a * b;
+                    }
+                    stack.push(res);
+                }
+            }
+        }
+        return stack.pop();
+    }
+    public double getPrevious(){
+        return 0.0;
+    }
 }
---
+++ src/pilas/IList.java
@@ -0,0 +1,58 @@
+package pilas;
+/*
+Interfaz que representa las caracteristicas necesarias de una para emplear en la lectura de datos
+*/
+
+import stack;
+
+public interface IList<T> {
+
+
+    /**
+     * post: returns true iff list has no elements
+     * @return
+     */
+    public boolean isEmpty();
+
+
+    /**
+     * pre: list is not empty
+     * post: returns last value in list
+     * @return
+     */
+    public T getLast();
+
+    /**
+     * pre: list is not empty
+     * post: returns first value in list
+     * @return
+     */
+    public T getFirst();
+
+
+    /**
+     * post: value is added to tail of list
+     * @param value
+     */
+    public void add(T value);
+
+
+    /**
+     * pre: list has at least one element
+     * post: removes last value found in list
+     * @return
+     */
+    public T remove();
+
+    /**
+     * post: returns the number of elements in the stack
+     *
+     * @return The number of elements in the stack.
+     */
+    public int size();
+
+
+
+
+}
+
