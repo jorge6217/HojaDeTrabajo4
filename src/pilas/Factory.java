ad29725cc75deafc050810ca7feccf1d1777423a
jorge6217 <jorge6217@gmail.com>
2023-02-22 21:53:55 -0600

    class num for implementOfStack and class Factory

---
+++ src/pilas/Factory.java
@@ -0,0 +1,11 @@
+package pilas;
+/*
+Implementa Arraylist, Vector, linkedList y DoubleLinkedList
+*/
+public class Factory <T> {
+
+    public IStack<T> getStack(ImplementationOfStack implementation){
+    	
+    }
+	
+}
